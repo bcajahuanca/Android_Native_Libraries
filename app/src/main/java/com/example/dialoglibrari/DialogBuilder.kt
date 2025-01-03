86ddd7d50ff53660312d67eb3e271c4c0b136eb8
bcajahuanca <bcajahuanca.abx@gmail.com>
2025-01-03 15:58:32 -0500

    Library finished

--- app/src/main/java/com/example/dialoglibrari/DialogBuilder.kt
+++ app/src/main/java/com/example/dialoglibrari/DialogBuilder.kt
@@ -3,13 +3,8 @@ package com.example.dialoglibrari
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
-import android.icu.text.CaseMap.Title
-import android.icu.text.ListFormatter.Width
-import android.os.Message
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.LinearLayout
 import android.widget.Toast
@@ -17,157 +12,267 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.example.dialoglibrari.databinding.CustomListDialogBinding
 import com.example.dialoglibrari.databinding.CustomSimpleDialogBinding
-import com.example.dialoglibrari.R.drawable.dialog_background
-import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener
-import org.w3c.dom.Text
 
 class DialogBuilder(private val context: Context) {
-    private var title: String? = null
-    private var titleSize: Float? = null
+    private var title: String? = "Default title"
+    private var titleSize: Float? = 14F
+    private var titleColor: Int? = Color.BLACK
 
-    private var description: String? = null
-    private var positiveButtonText: String? = null
-    private var negativeButtonText: String? = null
+    private var description: String? = "Here will be your description, please set one"
+    private var descriptionSize: Float? = 14F
+
+    private var positiveButtonText: String? = "Yes"
+    private var positiveButtonTextSize: Float? = 14F
+    private var positiveButtonColor: Int? = Color.WHITE
     private var positiveButtonClickListener: (() -> Unit)? = null
+
+    private var negativeButtonText: String? = null
+    private var negativeButtonTextSize: Float? = 14F
+    private var negativeButtonColor: Int? = Color.WHITE
     private var negativeButtonClickListener: (() -> Unit)? = null
+
     private var listItems: List<String>? = null
+    private var onItemClickListener: ((String) -> Unit)? = null
+
     private var dialog: AlertDialog? = null
 
-    private var backgroundColor:Int? = null
-    private var backgroundBorderColor: Int? = null
-    private var backgroundBorderWidth: Int? = null
+    private var backgroundColor:Int? = Color.WHITE
+    private var backgroundBorderColor: Int? = Color.GRAY
+    private var backgroundBorderWidth: Int? = 2
 
-    fun setTitleStyle(title: String?, size: Float?) = apply {
-        if (!title.isNullOrEmpty()){
-            this.title = title
-        }
-        if (size != null) {
-            this.titleSize = size
-        }
+    private var closeTouchOutSide: Boolean? = true
+
+    fun setTitleStyle(title: String? = this.title, size: Float? = this.titleSize, color: Int? = this.titleColor) = apply {
+        this.title = title
+        this.titleSize = size
+        this.titleColor = color
     }
-    fun setDescription(description: String) = apply { this.description = description }
-    fun setPositiveButton(text: String, listener: () -> Unit) = apply{
+
+    fun setDescription(description: String? = this.description, size: Float? = this.descriptionSize) = apply {
+        this.description = description
+        this.descriptionSize = size
+    }
+
+    fun setPositiveButton(text: String? = this.positiveButtonText, size: Float? = this.positiveButtonTextSize, color: Int? = this.positiveButtonColor, listener: () -> Unit) = apply{
         this.positiveButtonText = text
+        this.positiveButtonTextSize = size
+        this.positiveButtonColor = color
         this.positiveButtonClickListener = listener
     }
-    fun setNegativeButton(text: String, listener: () -> Unit) = apply{
+
+    fun setNegativeButton(text: String? = this.negativeButtonText, size: Float? = this.negativeButtonTextSize, color: Int? = this.negativeButtonColor, listener: () -> Unit) = apply{
         this.negativeButtonText = text
+        this.negativeButtonTextSize = size
+        this.negativeButtonColor = color
         this.negativeButtonClickListener = listener
     }
+
     fun setListItems(items: List<String>) = apply { this.listItems = items }
 
+    fun seOnItemClickListener(listener: (String) -> Unit) = apply { this.onItemClickListener = listener }
 
-    fun setBackgroundColor(colorBackground: String? = null, colorBorder: String? = null, borderWidth: Int? = null) = apply {
-        if (colorBackground != null) {
-            this.backgroundColor = Color.parseColor(colorBackground)
-        }
-        if (colorBorder != null) {
-            this.backgroundBorderColor = Color.parseColor(colorBorder)
-        }
-        if (borderWidth != null) {
-            this.backgroundBorderWidth = borderWidth
+    fun setBackgroundColor(colorBackground: Int? = this.backgroundColor, colorBorder: Int? = this.backgroundBorderColor, borderWidth: Int? = this.backgroundBorderWidth) = apply {
+        this.backgroundColor = colorBackground
+        this.backgroundBorderColor = colorBorder
+        this.backgroundBorderWidth = borderWidth
+    }
+
+    fun isEasilyCloseAble(close: Boolean?= null) = apply { this.closeTouchOutSide = close }
+
+    private fun setStyleBackground(binding: Any?) {
+        val backgroundDrawable = ContextCompat.getDrawable(context, R.drawable.dialog_background) as? GradientDrawable
+        backgroundDrawable?.let { drawable ->
+            backgroundColor?.let { drawable.setColor(it) }
+            if (backgroundBorderColor != null && backgroundBorderWidth != null) {
+                drawable.setStroke(backgroundBorderWidth!!, backgroundBorderColor!!)
+            }
+            when (binding) {
+                is CustomListDialogBinding -> binding.root.background = drawable
+                is CustomSimpleDialogBinding -> binding.root.background = drawable
+            }
         }
     }
 
-    fun build() {
-        if (listItems.isNullOrEmpty()) {
-            buildSimpleDialog()
-        } else {
-            buildListDialog()
-        }
-    }
+    private fun setTitleStyleBuild(binding: Any?){
+        val defaultTitle: String = "Default Title"
+        val defaultSizeTitle: Float = 8F
 
-    fun setStyleBackground(bindingList: CustomListDialogBinding?, bindingSimple: CustomSimpleDialogBinding?){
-        val backgroundDrawable = ContextCompat.getDrawable(context, R.drawable.dialog_background)
-        if (backgroundDrawable is GradientDrawable) {
-            backgroundColor?.let {
-                backgroundDrawable.setColor(it)
-                backgroundBorderColor?.let { it1 -> backgroundBorderWidth?.let { it2 ->
-                    backgroundDrawable.setStroke(
-                        it2, it1)
-                } }
-                if (bindingSimple != null) {
-                    bindingSimple.root.background = backgroundDrawable
-                } else if (bindingList != null) {
-                    bindingList.root.background = backgroundDrawable
+        when (binding) {
+            is CustomListDialogBinding -> {
+                binding.dialogTitle.text = title?: defaultTitle
+                binding.dialogTitle.textSize = titleSize ?: defaultSizeTitle
+                titleColor?.let {
+                    binding.dialogTitle.setTextColor(it)
+                }
+            }
+            is CustomSimpleDialogBinding -> {
+                binding.dialogTitle.text = title?: defaultTitle
+                binding.dialogTitle.textSize = titleSize ?: defaultSizeTitle
+                titleColor?.let {
+                    binding.dialogTitle.setTextColor(it)
                 }
             }
         }
     }
 
 
+    private fun setDescriptionStyle(binding: Any?) {
+        val defaultDescription = "Default description, please set your own description"
+        val defaultSize = 8F
 
-    private fun buildSimpleDialog() {
-        val binding = CustomSimpleDialogBinding.inflate(LayoutInflater.from(context))
-        binding.dialogTitle.text = title?: "Default Title"
-        binding.dialogDescription.text = description?: "Default description, please set your own description"
-
-        setStyleBackground(null,binding)
-
-        dialog = AlertDialog.Builder(context)
-            .setView(binding.root)
-            .create()
-
-        binding.dialogTitle.textSize = titleSize ?: 8F
-        binding.dialogButton.text = positiveButtonText ?: "Aceptar"
-        binding.dialogButton.setOnClickListener {
-            positiveButtonClickListener?.invoke()
-        }
-        dialog?.setCanceledOnTouchOutside(false)
-        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
-        dialog?.show()
-    }
-
-    private fun buildListDialog() {
-        val binding = CustomListDialogBinding.inflate(LayoutInflater.from(context))
-        binding.dialogTitle.text = title?: "Default Title"
-        binding.dialogDescription.text = description?: "Default description, please set your own description"
-
-        setStyleBackground(binding,null)
-
-        listItems?.let {
-            val adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, it)
-            binding.dialogList.adapter = adapter
-            if (adapter.count > 4) {
-                 val itemView = adapter.getView(0, null, binding.dialogList)
-                itemView.measure(0,0)
-                val itemHeight = itemView.measuredHeight
-
-                val maxHeight = (4.3 * itemHeight).toInt()
-                binding.dialogList.layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    maxHeight
-                )
+        when (binding) {
+            is CustomListDialogBinding -> {
+                binding.dialogDescription.text = description ?: defaultDescription
+                binding.dialogDescription.textSize = descriptionSize ?: defaultSize
+            }
+            is CustomSimpleDialogBinding -> {
+                binding.dialogDescription.text = description ?: defaultDescription
+                binding.dialogDescription.textSize = descriptionSize ?: defaultSize
             }
         }
+    }
 
-        binding.dialogList.setOnItemClickListener { _, _, position, _ ->
-            val selectedItem = listItems?.get(position)
-            Toast.makeText(context, "Empresa seleccionada: $selectedItem", Toast.LENGTH_LONG).show()
+    private fun setPositiveButtonStyleActions(binding: Any?) {
+        val defaultTextYes: String = "Yes"
+        val defaultSize: Float = 10F
+        val backgroundDrawable = ContextCompat.getDrawable(context, R.drawable.rounded_button_positive) as? GradientDrawable
+
+        backgroundDrawable?.let {
+            positiveButtonColor?.let { backgroundDrawable.setColor(it) }
         }
 
-        dialog = AlertDialog.Builder(context)
-            .setView(binding.root)
-            .create()
+        when(binding) {
+            is CustomSimpleDialogBinding -> {
+                binding.dialogButtonYes.text = positiveButtonText ?: defaultTextYes
+                binding.dialogButtonYes.textSize = positiveButtonTextSize ?: defaultSize
+                binding.dialogButtonYes.setOnClickListener {
+                    positiveButtonClickListener?.invoke()
+                }
+                binding.dialogButtonYes.background = backgroundDrawable
+            }
+            is CustomListDialogBinding -> {
+                binding.dialogButtonYes.text = positiveButtonText ?: defaultTextYes
+                binding.dialogButtonYes.textSize = positiveButtonTextSize ?: defaultSize
+                binding.dialogButtonYes.setOnClickListener {
+                    positiveButtonClickListener?.invoke()
+                }
+                binding.dialogButtonYes.background = backgroundDrawable
+            }
+        }
+    }
 
-        binding.dialogButtonYes.text = positiveButtonText ?: "Aceptar"
-        binding.dialogButtonYes.setOnClickListener {
-            positiveButtonClickListener?.invoke()
+    private fun setNegativeButtonStyleActions(binding: Any?) {
+        val defaultTextNo: String = "No"
+        val defaultSize: Float = 10F
+        val backgroundDrawable = ContextCompat.getDrawable(context, R.drawable.rounded_button_negative) as? GradientDrawable
+
+        backgroundDrawable?.let {
+            negativeButtonColor?.let { backgroundDrawable.setColor(it) }
+        }
+
+        when(binding) {
+            is CustomSimpleDialogBinding -> {
+                binding.dialogButtonNo.text = negativeButtonText ?: defaultTextNo
+                binding.dialogButtonNo.textSize = negativeButtonTextSize ?: defaultSize
+                binding.dialogButtonNo.setOnClickListener {
+                    negativeButtonClickListener?.invoke()
+                }
+                binding.dialogButtonNo.background = backgroundDrawable
+            }
+            is CustomListDialogBinding -> {
+                binding.dialogButtonNo.text = negativeButtonText ?: defaultTextNo
+                binding.dialogButtonNo.textSize = negativeButtonTextSize ?: defaultSize
+                binding.dialogButtonNo.setOnClickListener {
+                    negativeButtonClickListener?.invoke()
+                }
+                binding.dialogButtonNo.background = backgroundDrawable
+            }
+        }
+    }
+
+    fun build() {
+//        if (listItems.isNullOrEmpty()) {
+//            buildSimpleDialog()
+//        } else {
+//            buildListDialog()
+//        }
+        buildDialog()
+    }
+
+//    private fun buildSimpleDialog() {
+//        val binding = CustomSimpleDialogBinding.inflate(LayoutInflater.from(context))
+//        setTitleStyleBuild(binding)
+//        setStyleBackground(binding)
+//        setDescriptionStyle(binding)
+//
+////        binding.dialogButtonYes.text = positiveButtonText ?: "Aceptar"
+////        binding.dialogButtonYes.setOnClickListener {
+////            positiveButtonClickListener?.invoke()
+////        }
+//
+//        setPositiveButtonStyleActions(binding)
+//
+//        dialog = AlertDialog.Builder(context)
+//            .setView(binding.root)
+//            .create()
+//
+//        dialog?.setCanceledOnTouchOutside(false)
+//        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+//        dialog?.show()
+//    }
+
+
+
+    private fun buildDialog() {
+        val binding = CustomListDialogBinding.inflate(LayoutInflater.from(context))
+
+        setTitleStyleBuild(binding)
+        setStyleBackground(binding)
+        setDescriptionStyle(binding)
+        setPositiveButtonStyleActions(binding)
+
+        if (listItems != null) {
+            listItems?.let {
+                val adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, it)
+                binding.dialogList.adapter = adapter
+                if (adapter.count > 4) {
+                    val itemView = adapter.getView(0, null, binding.dialogList)
+                    itemView.measure(0,0)
+                    val itemHeight = itemView.measuredHeight
+
+                    val maxHeight = (4.3 * itemHeight).toInt()
+                    binding.dialogList.layoutParams = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        maxHeight
+                    )
+                }
+            }
+            binding.dialogList.setOnItemClickListener { _, _, position, _ ->
+                val selectedItem = listItems?.get(position)
+                selectedItem?.let {
+                    onItemClickListener?.invoke(it)
+                }
+            }
+        } else {
+            binding.dialogList.visibility = View.GONE
         }
 
         if (negativeButtonText != null) {
-            binding.dialogButtonNo.text = negativeButtonText
-            binding.dialogButtonNo.setOnClickListener {
-                negativeButtonClickListener?.invoke()
-            }
-            binding.dialogButtonNo.visibility = View.VISIBLE
+            setNegativeButtonStyleActions(binding)
         } else {
             binding.dialogButtonNo.visibility = View.GONE
         }
-        dialog?.setCanceledOnTouchOutside(false)
+
+
+        dialog = AlertDialog.Builder(context)
+            .setView(binding.root)
+            .create()
+
+        dialog?.setCanceledOnTouchOutside(closeTouchOutSide ?: false)
         dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
         dialog?.show()
     }
+
     fun dismissDialog(){
         dialog?.dismiss()
     }
--- app/src/main/java/com/example/dialoglibrari/MainActivity.kt
+++ app/src/main/java/com/example/dialoglibrari/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.dialoglibrari
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.dialoglibrary.DialogManager
+import com.example.dialoglibrari.databinding.ActivityMainBinding
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
@@ -12,28 +13,65 @@ class MainActivity : AppCompatActivity() {
         "Etul 4", "El Rapido","Cruz del Centro", "Etul 4", "El Rapido","Cruz del Centro", "Etul 4",
         "El Rapido","Cruz del Centro", "Etul 4", "El Rapido","Cruz del Centro", "Etul 4", "El Rapido",
         "Cruz del Centro", "Etul 4", "El Rapido","Cruz del Centro", "Etul 4", "El Rapido")
-//    private val routes : List<String> = listOf("Cruz del Centro", "Etul 4", "El Rapido")
+    private val routesShort : List<String> = listOf("Cruz del Centro", "Etul 4", "El Rapido", "Corredor Morado")
 
     private var dialogBuilder: DialogBuilder? = null
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        dialogBuilder = DialogBuilder(this)
-            .setTitleStyle(title = "Confirma la compra?", size = 30F)
-            .setDescription("¿Está seguro que desea continuar?")
-            .setPositiveButton("Sí"){
-                dialogBuilder?.dismissDialog()
-                Toast.makeText(this, "Continuando....", Toast.LENGTH_LONG).show()
-            }
-            .setBackgroundColor(colorBackground = "#ffffff", colorBorder = "#028000", borderWidth = 10)
+        binding.buttonSimpleDialog.setOnClickListener {
+            dialogBuilder = DialogBuilder(this)
+                .setTitleStyle(title = "¿Confirmar?", size = 30F)
+                .setDescription("¿Está seguro que desea continuar?", 20F)
+                .setPositiveButton("Yes", 10F, Color.GREEN){
+                    dialogBuilder?.dismissDialog()
+                    Toast.makeText(this, "Continuando....", Toast.LENGTH_LONG).show()
+                }
+                .setBackgroundColor(borderWidth = 5)
+                .isEasilyCloseAble(true)
 
-        dialogBuilder?.build()
+            dialogBuilder?.build()
+        }
+
+        binding.buttonListDialog.setOnClickListener {
+            dialogBuilder =  DialogBuilder(this)
+                .setTitleStyle("Confirmación", 30F, Color.BLACK)
+                .setDescription("¿Está seguro que desea continuar?", 20F)
+                .setPositiveButton("Sí", 10F, Color.GREEN){
+                    Toast.makeText(this, "Continuando....", Toast.LENGTH_LONG).show()
+                }
+                .setNegativeButton("No", 15F, Color.RED) {
+                    dialogBuilder?.dismissDialog()
+                    println("CERRANDO DIALOOOOOOOGGGGGGG")
+                }
+                .setListItems(routes)
+                .seOnItemClickListener { selectedItem ->
+                    Toast.makeText(this, "Seleccionaste $selectedItem", Toast.LENGTH_LONG).show()
+                }
+                .setBackgroundColor(colorBackground = Color.GRAY, borderWidth = 10)
+
+            dialogBuilder?.build()
+        }
+
+
+//        dialogBuilder = DialogBuilder(this)
+//            .setTitleStyle(title = "Confirma la compra?", size = 30F, color = "000000")
+//            .setDescription("¿Está seguro que desea continuar?")
+//            .setPositiveButton("Sí"){
+//                dialogBuilder?.dismissDialog()
+//                Toast.makeText(this, "Continuando....", Toast.LENGTH_LONG).show()
+//            }
+//            .setBackgroundColor(colorBackground = "ffffff", colorBorder = "028000", borderWidth = 5)
+//
+//        dialogBuilder?.build()
 
 //        dialogBuilder =  DialogBuilder(this)
-//            .setTitle("Confirmación")
-//            .setDescription("¿Está seguro que desea continuar?")
+//            .setTitleStyle("Confirmación", 30F, "000000")
+//            .setDescription("¿Está seguro que desea continuar?", 20F)
 //            .setPositiveButton("Sí"){
 //                Toast.makeText(this, "Continuando....", Toast.LENGTH_LONG).show()
 //            }
@@ -42,7 +80,7 @@ class MainActivity : AppCompatActivity() {
 //                println("CERRANDO DIALOOOOOOOGGGGGGG")
 //            }
 //            .setListItems(routes)
-//            .setBackgroundColor(colorBackground = "#ffffff", colorBorder = "#028000", borderWidth = 10)
+//            .setBackgroundColor(colorBackground = "ffffff", colorBorder = "028000", borderWidth = 10)
 ////            .setBackgroundColor("#ffffff", "#028000", 50)
 //
 //        dialogBuilder?.build()
